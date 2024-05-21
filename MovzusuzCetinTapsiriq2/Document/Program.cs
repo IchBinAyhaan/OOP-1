4f5227926b35d2028c13ae358e3916d192898c60
IchBinAyhaan <165468545+IchBinAyhaan@users.noreply.github.com>
2024-05-21 23:08:52 +0400

    Update Program.cs

--- MovzusuzCetinTapsiriq2/Document/Program.cs
+++ MovzusuzCetinTapsiriq2/Document/Program.cs
@@ -4,11 +4,11 @@
     {
         static void Main(string[] args)
         {
-            // Kullanıcıdan seçim alınır
+            
             Console.WriteLine("Choose a program type (basic, pro, expert):");
             string programType = Console.ReadLine();
 
-            // Seçime göre ilgili sınıfın nesnesi oluşturulur
+           
             DocumentProgram program;
             switch (programType)
             {
@@ -26,7 +26,7 @@
                     return;
             }
 
-            // Metodlar çağrılır
+      
             program.OpenDocument();
             program.EditDocument();
             program.SaveDocument();
